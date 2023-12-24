49933a6c726c5784e4402d7a94d644e2002aa7a7
Yuri Tischenko <yuri.tishchenko@gmail.com>
2023-12-24 13:03:10 +0300

    Change return True if re.match() else False in Validators

--- yacut/validators.py
+++ yacut/validators.py
@@ -4,11 +4,11 @@ from .constants import EMPTY_PATTERN, PATTERN_CUSTOM_ID, URL_REGEX
 
 
 def empty_id(id):
-    return re.match(EMPTY_PATTERN, id)
+    return True if re.match(EMPTY_PATTERN, id) else False
 
 def is_url(link):
-    return re.match(URL_REGEX, link)
+    return True if re.match(URL_REGEX, link) else False
 
 def check_id(id):
-    return re.match(PATTERN_CUSTOM_ID, id)
+    return True if re.match(PATTERN_CUSTOM_ID, id) else False
  
